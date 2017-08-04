6c91ca065162ea4c3e9d44f2fc48cdcaa8c089d6
DebbiePao <dpao@berkeley.edu>
2017-08-04 12:30:21 -0700

    final commit

--- routes/connectingDatabase.js
+++
@@ -1,9 +0,0 @@
-function() {
-    const connection = mysql.createConnection({
-    host: 'ankurmgoyal.ccfuvi1hkijt.us-west-2.rds.amazonaws.com',
-    port: '3306',
-    user: 'ankurmgoyal',
-    password: 'tab1et!p',
-    database: 'dexterMVP'
-  });
-}
