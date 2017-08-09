08c2eee2e8fb2cc214d6a02ba5528053c23df3ae
DebbiePao <dpao@berkeley.edu>
2017-08-09 11:49:45 -0700

    changing

--- routes/index.js
+++ routes/index.js
@@ -48,12 +48,12 @@ router.post('/login', (req, res) => {
   var usernameInput = req.body.username;
   var passwordInput = req.body.password;
   var salt = CryptoJS.lib.WordArray.random(128/8);
-  var key128Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 128/32 });
-  var key256Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 256/32 });
-  var key512Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 512/32 });
-  var key512Bits1000Iterations = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 512/32, iterations: 1000 });
-  var ciphertext = CryptoJS.PBKDF2.encrypt(passwordInput, key512Bits1000Iterations);
-  console.log('PASS', ciphertext);
+  // var key128Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 128/32 });
+  // var key256Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 256/32 });
+  // var key512Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 512/32 });
+  var key512Bits1000Iterations = CryptoJS.PBKDF2(passwordInput, salt, { keySize: 512/32, iterations: 1000 });
+  // var ciphertext = CryptoJS.PBKDF2.encrypt(passwordInput, 'secret key 123');
+  console.log('PASS', key512Bits1000Iterations);
   const tableName = 'Users';
   const sql = 'SELECT locationID, employeeID FROM ?? WHERE username = ?';
   connection.query(sql, [tableName, usernameInput], (error, results, fields) => {
