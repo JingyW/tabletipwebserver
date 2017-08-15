42578868bb91f0efe734fdd9bad0118c9802f15a
JingyW <wangj2@whitman.edu>
2017-08-15 14:04:10 -0700

    change from boolean to string

--- routes/index.js
+++ routes/index.js
@@ -87,14 +87,14 @@ router.post('/login', (req, res) => {
 });
 
 router.post('/firstLogin', (req, res) => {
+	console.log("inside firstlogin")
   var passwordInput = req.body.password;
   var salt = 'salt'; //CryptoJS.lib.WordArray.random(128/8)
   var userHash = CryptoJS.PBKDF2(passwordInput, salt, { keySize: 512/32, iterations: 1000 });
 	// const updatefirstTime = 'UPDATE Users SET firstTime = ? WHERE username = ?';
 	//const defaultPass = '1000:a49359efa98f87a43580ccaaf14e6a145e78425a08c578f2:21eea828ae679948aac2b486ec26fa840e88eaa25141338d';
 	const updatePass = 'UPDATE Users SET password = ?, firstTime = ? WHERE username = ?';
-	console.log(updatePass)
-	connection.query(updatePass,[userHash, false, 'goelv'], (error, result, fields) => {
+	connection.query(updatePass,[userHash, 'false', 'goelv'], (error, result, fields) => {
 		if (error) {
 			console.log('Error: ' + error);
 			res.json({success: false});
